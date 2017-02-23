c4747ccb3a6a575d2ddf88c9a7452df760b4e68c
nilq <i.am@nilq.dk>
2017-02-23 19:15:41 +0100

    Add rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+extern crate ggez;
+
+use ggez::conf;
+use ggez::event;
+use ggez::{GameResult, Context};
+use ggez::graphics;
+use ggez::timer;
+use std::time::Duration;
+
+struct MainState {
+    rect: graphics::Rect,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let rect = graphics::Rect::new(100, 100, 100, 100);
+
+        let s = MainState {
+            rect: rect
+        };
+
+        Ok(s)
+    }
+}
+
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context, _dt: Duration) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        ctx.renderer.clear();
+        graphics::rectangle(ctx, graphics::DrawMode::Fill, self.rect)?;
+        ctx.renderer.present();
+        timer::sleep_until_next_frame(ctx, 60);
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("helloworld", c).unwrap();
+    let state = &mut MainState::new(ctx).unwrap();
+
+    if let Err(e) = event::run(ctx, state) {
+        println!("Error encountered: {}", e);
+    } else {
+        println!("Game exited cleanly.");
+    }
 }
